e928ab96fcc2d0324c002b3baf2e515e7f7cf4ee
AneriMehat <44049491+AneriMehat@users.noreply.github.com>
2018-10-11 11:59:39 +0530

    Add files via upload

---
+++ filter2/src/java/Myfilter.java
@@ -0,0 +1,44 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import static java.util.Locale.filter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class Myfilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        
+        PrintWriter out=response.getWriter();
+        out.println("Filter Started");
+        chain.doFilter(request, response);
+        out.println("Filter Ended");
+        
+        
+//To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void destroy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
